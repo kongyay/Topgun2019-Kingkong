a2ddf9357ac8e1b9cdc0bb8b0af60f575c5b5963
Kamonnop Arunrat <kamonnop@opsta.co.th>
2019-01-08 15:03:33 +0700

    create api showByID

--- server/day2-test/server.js
+++ server/day2-test/server.js
@@ -12,7 +12,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send("KINGKONG");
-})
+});
+
+app.get('/listusers', (req, res) => {
+  fs.readFile(__dirname + "/" + "users.json", 'utf8', (err, data) => {
+    console.log(data);
+    res.end(data);
+  });
+});
+
+app.get('/showbyID/:id', (req, res) => {
+  fs.readFile(__dirname + "/" + "users.json", 'utf8', (err, data) => {
+    let id = parseInt(req.params.id);    
+    data = JSON.parse(data);
+    
+    let resultKey = Object.keys(data).find(x => data[x].id == id);
+
+    if (resultKey != undefined) {
+      let result = {};
+      result[resultKey] = data[resultKey];
+      res.end(JSON.stringify(result));
+    } else {
+      res.end("NOT FOUND");
+    }
+  });
+});
 
 let server = app.listen(3000, () => {
   let port = server.address().port;
