751fa4f2c7734a6cb0a54249e2aa67d93b80eee9
Pitawat K <58010886@kmitl.ac.th>
2019-01-09 16:16:40 +0700

    Day3 predict from csv

--- tensorflow/Day3/predict.js
+++ tensorflow/Day3/predict.js
@@ -1,18 +1,37 @@
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
 
+var data = [];
+
+// READ
+function readCSV() {
+    data = []
+    var fs = require('fs');
+    var fileContents = fs.readFileSync('./THB_input.csv');
+    var lines = fileContents.toString().split('\n');
+
+    for (var i = 0; i < lines.length; i++) {
+        var subline = lines[i].toString().split(',');
+        var corrected = [new Date(subline[0]+subline[1]).getTime(),parseFloat(subline[2])]
+        data.push(corrected);
+    }
+}
 
 async function main(){
     // PREDICTION
     const model = await tf.loadModel('file://model/model.json');
+    readCSV()
+    
+    for(let i=0;i<data.length;i++) {
+        let xx = [[data[i][0]]];
+        let xxx = tf.tensor2d(xx);
+        xxx = tf.reshape(xxx,[1,1,1]);
 
-    let xx = [[1533229200000]];
-    let xxx = tf.tensor2d(xx);
-    xxx = tf.reshape(xxx,[1,1,1]);
+        const r = model.predict(xxx);
+        let result = r.dataSync()[0];
+        console.log(data[i][0],'=>',result,"Actual:",data[i][1]);
+    }
 
-    const r = model.predict(xxx);
-    let result = r.dataSync()[0];
-    console.log('RESULT',result);
 }
 
 main();
