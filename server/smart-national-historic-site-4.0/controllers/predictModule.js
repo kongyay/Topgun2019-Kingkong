3a8aa9e074ea3ca22049b90dd76c148c3f17522a
Kamonnop Arunrat <kamonnop@opsta.co.th>
2019-01-10 22:30:13 +0700

    edit model

--- server/smart-national-historic-site-4.0/controllers/predictModule.js
+++ server/smart-national-historic-site-4.0/controllers/predictModule.js
@@ -1,9 +1,10 @@
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
+const maxmin = require('./maxmin.json')
 
 var data = [];
-const MAXY = 36.50
-const MINY = 28.615
+const MAXY = maxmin[0]
+const MINY = maxmin[1]
 // READ
 
 function denorm(result){
@@ -11,23 +12,23 @@ function denorm(result){
 }
 // [1,2,3,4,5,6,7,8,9,10,11,12]
 module.exports = async (data) => {
-  // CSV DAYS
-  const model = await tf.loadModel('file://models/model.json');
+    // CSV DAYS
+    const model = await tf.loadModel('file://models/model.json');
 
-  // PREPARE
-  data = data.map((yi) => {
-    return parseFloat(((yi-MINY)/(MAXY-MINY)).toFixed(4));
-  })
+    // PREPARE
+    data = data.map((yi) => {
+        return parseFloat(((yi-MINY)/(MAXY-MINY)).toFixed(4));
+    })
     
-  let xx = [data];
-  //console.log(xx[0].map((xi)=> denorm(xi)))
-  let xxx = tf.tensor2d(xx);
-  xxx = tf.reshape(xxx,[1,12,1]);
+    let xx = [data];
+    //console.log(xx[0].map((xi)=> denorm(xi)))
+    let xxx = tf.tensor2d(xx);
+    xxx = tf.reshape(xxx,[1,12,1]);
 
-  const r = model.predict(xxx);
-  let result = r.dataSync();
-  n_result = result.map(r => denorm(r))
-  //console.log(data[i][0],'=>',n_result);
-  return n_result
+    const r = model.predict(xxx);
+    let result = r.dataSync();
+    n_result = result.map(r => denorm(r))
+    //console.log(data[i][0],'=>',n_result);
+    return n_result
 
 }
