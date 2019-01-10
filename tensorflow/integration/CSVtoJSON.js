4b7a8fae13c794701bc4aec0c8e08da22e0bea02
Pitawat K <58010886@kmitl.ac.th>
2019-01-10 15:57:02 +0700

    add Module for server to use ML

---
+++ tensorflow/integration/CSVtoJSON.js
@@ -0,0 +1,40 @@
+
+var fs = require('fs');
+var data = []
+
+var fileContents = fs.readFileSync('./sanam.csv');
+var lines = fileContents.toString().split('\n');
+
+for (var i = 1; i < lines.length; i++) {
+    var subline = lines[i].toString().split(';');
+    //console.log(subline)
+    for(var k = 1;k < subline.length; k++) {
+        var hourly = {
+            "timestamp": `${subline[0]}T${(k-1<10? '0':'') + (0+k-1)}:00:00:000Z`,
+            "P-IN": parseInt(subline[k]),
+            "P-OUT": 0
+        }
+        data.push(hourly)
+    }
+    
+    
+
+}
+
+console.log(data)
+
+
+fs.writeFile("bigchunkus.json", JSON.stringify(data), function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
+
+/*
+{
+    timestamp: "YYYY-MM-DDTHH:00:00:000Z",
+    P-IN: xxx,
+    P-OUT: 0
+}
+
+*/
